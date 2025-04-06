9e21f4bb600aae839f7624dfe638d03d7539144e
kikuchi <smj320@gmail.com>
2025-04-06 13:15:52 +0900

    圧縮率を変更

--- pyUSBcam.sh
+++ pyUSBcam.sh
@@ -3,7 +3,7 @@ do
   DD=`date '+%Y-%m-%d_%H:%M:%S'`
   FN_L=/home/admin/proj/pyUSBcam/img_l/$DD.jpg
   FN_S=/home/admin/proj/pyUSBcam/img_s/current.jpg
-  /usr/bin/fswebcam 640x480 --jpeg 50 $FN_L
+  /usr/bin/fswebcam 640x480 $FN_L
   /usr/bin/fswebcam 640x480 --jpeg 50 $FN_S
   /usr/bin/python /home/admin/proj/pyUSBcam/encode.py
 done
