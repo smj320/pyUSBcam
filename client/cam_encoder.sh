6e8d17f02b9284d66248ae469a8deacdfe071214
kikuchi <smj320@gmail.com>
2025-05-05 19:23:49 +0900

    encodeとなっているところがあったのでencoderに変更2

--- client/cam_encoder.sh
+++ client/cam_encoder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_ROOT=/home/admin/pyUSBcam
 TS=$(date "+%Y%m%d%H%M%S")
-LOG=${PROJECT_ROOT}/log/${TS}-encode.log
+LOG=${PROJECT_ROOT}/log/${TS}-encoder.log
 # shellcheck disable=SC2164
 cd ${PROJECT_ROOT}/client
 . ../.venv/bin/activate
