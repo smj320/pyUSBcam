67130d57e9e7e3d76a89b653ff84964b97ff22ef
kikuchi <smj320@gmail.com>
2025-05-05 12:57:35 +0900

    テスト追加

---
+++ client/cam_test.py
@@ -0,0 +1,37 @@
+"""
+mac用。フレームシンクワードで見分けて、jpegをシリアルからダンプする。
+pip pySerial
+pip install opencv-python
+pip install matplotlib
+"""
+import time
+import cv2
+
+def main():
+
+    src = 'v4l2src device=/dev/video0 ! image/jpeg,'
+    src += 'width=1920, height=1080,'
+    src += 'framerate=(fraction)30/1 !jpegdec !videoconvert ! appsink'
+    # UVCカメラを開く。macでは、0はiPhoneのカメラとかになるので,デスクトップカメラは1
+    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(src)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    ww = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    hh = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print("Default")
+    print(fps, ww, hh)
+
+    fps = cap.set(cv2.CAP_PROP_FPS, 5)
+    ww = cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    hh = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    print("Sset")
+    print(fps, ww, hh)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    ww = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    hh = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    print("Result")
+    print(fps, ww, hh)
+
+if __name__ == "__main__":
+    main()
