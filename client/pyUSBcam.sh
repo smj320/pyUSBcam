8a56a6ecc49646d68a64c38c20873ebe80d52273
kikuchi <smj320@gmail.com>
2025-05-05 16:43:34 +0900

    起動シェルを改良2

--- client/pyUSBcam.sh
+++ client/pyUSBcam.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
-# PROJECT_ROOT=/home/admin/pyUSBcam
-PROJECT_ROOT=/Users/kikuchi/Projects/PycharmProjects/pyUSBcam
+PROJECT_ROOT=/home/admin/pyUSBcam
+# PROJECT_ROOT=/Users/kikuchi/Projects/PycharmProjects/pyUSBcam
 LOG=$PROJECT_ROOT/log
 TS=$(date "+%Y%m%d%H%M%S")
 # shellcheck disable=SC2164
 echo $PROJECT_ROOT/client
 # shellcheck disable=SC2164
 cd $PROJECT_ROOT/client
-find img -name '*.jpg' -delete
-find sent -name '*.jpg' -delete
+find img -name '*.jpg' -delete  > $LOG/$TS-client.log
+find sent -name '*.jpg' -delete  >> $LOG/$TS-client.log
 . ../.venv/bin/activate
 python3 capture.py > $LOG/$TS-capture.log &
 # sleep 5
-python3 encoder.py
+python3 encoder.py >> $LOG/$TS-client.log
