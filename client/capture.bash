36b6e581775e9f8a64bfd5c65dfebff8f3af6a29
kikuchi <smj320@gmail.com>
2025-05-29 20:29:44 +0900

    capture.bashでimgを削除するのを止める。

--- client/capture.bash
+++ client/capture.bash
@@ -4,10 +4,7 @@ TS=$(date "+%Y%m%d%H%M%S")
 LOG=${PROJECT_ROOT}/log/${TS}-capture.log
 # shellcheck disable=SC2164
 cd ${PROJECT_ROOT}/client
-find img -name '*.jpg' -delete  &> ${LOG}
-find sent -name '*.jpg' -delete  &>> ${LOG}
 . ../.venv/bin/activate
-sleep 60
 if [ "$1" == "-v" ]; then
   python3 capture.py
 else
